c37f9cd4031ebe07000fac7f0ac486b750582c32
kk <405437445@qq.com>
2024-05-26 19:30:31 +0800

    rename scripts per-device
    Signed-off-by: kk <405437445@qq.com>

---
+++ generic.sh
@@ -0,0 +1,140 @@
+#! /bin/bash
+filename=$1
+source_img_name=${filename%.*}
+#source_img_file="${source_img_name}.img.gz"
+#target_img_name="JELOS-RGB30.aarch64-20240314-MOU"
+mount_point="target"
+common_files="common-files"
+system_root="SYSTEM-root"
+systemd_path="${system_root}/usr/lib/systemd/system"
+config_path="${system_root}/usr/config"
+modules_load_path="${system_root}/usr/lib/modules-load.d"
+
+if [ -z "$1" ]  
+then  
+    echo "Should run with img as: sudo ./jelos.sh xxx.img"
+    exit 1
+fi
+
+# Check if root
+if [ "$UID" -ne 0 ]; then
+    echo "The script should be run with sudo!!!" >&2
+    exit 1
+fi
+
+echo "Welcome to build Jelos for Generic Device!"
+
+echo "Creating mount point"
+mkdir ${mount_point}
+echo "Mounting Jelos boot partition"
+loop_device=$(losetup -f)
+losetup -P $loop_device $1
+mount ${loop_device}p3 ${mount_point}
+
+echo "Decompressing SYSTEM image"
+rm -rf ${system_root}
+unsquashfs -d ${system_root} ${mount_point}/SYSTEM
+
+# Add roms partition
+echo "Update fs-resze file"
+sudo cp -f ${common_files}/fs-resize_jelos ${system_root}/usr/lib/jelos/fs-resize 
+
+
+sudo cp -f ${common_files}/010-autorun ${system_root}/usr/lib/autostart/cbepx/
+sudo cp -f ${common_files}/007-rootpw ${system_root}/usr/lib/autostart/common/
+
+echo "Support openborff"
+cp -f ${common_files}/openbor/OpenBOR_ff  ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/OpenBOR_ff
+cp -f ${common_files}/openbor/start_OpenBOR.sh  ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/start_OpenBOR.sh
+cp -f ${common_files}/openbor/master*  ${system_root}/usr/config/openbor/
+
+echo "Optimize system startup"
+cp -f ${common_files}/automount ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/automount
+
+echo "Fix pico8"
+cp -f ${common_files}/start_pico8.sh ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/start_pico8.sh
+
+echo "Update smb.conf"
+cp -f ${common_files}/smb.conf ${system_root}/usr/config/
+
+echo "Fix flycast"
+cp -f ${common_files}/flycast ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/flycast
+cp -f ${common_files}/start_flycast.sh ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/start_flycast.sh
+
+echo "Optimize bluetooth"
+rm -rf ${system_root}/usr/lib/systemd/system/bluetoothsense.service
+cp -f ${common_files}/batocera-bluetooth ${system_root}/usr/bin/batocera-bluetooth
+chmod 775 ${system_root}/usr/bin/batocera-bluetooth
+cp -f ${common_files}/jelos-bluetooth ${system_root}/usr/bin/jelos-bluetooth
+chmod 775 ${system_root}/usr/bin/jelos-bluetooth
+cp -f ${common_files}/bluetoothctl ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/bluetoothctl
+cp -f ${common_files}/bt.sh ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/bt.sh
+
+echo "Update j2me files"
+cp -f ${common_files}/freej2me-linux-aarch64.jar ${system_root}/usr/config/game/freej2me/freej2me-linux-aarch64.jar
+chmod 775 ${system_root}/usr/config/game/freej2me/freej2me-linux-aarch64.jar
+cp -f ${common_files}/runemu.sh ${system_root}/usr/bin/runemu.sh
+chmod 775 ${system_root}/usr/bin/runemu.sh
+cp -f ${common_files}/sdl_interface ${system_root}/usr/bin/sdl_interface
+chmod 775 ${system_root}/usr/bin/sdl_interface
+cp -f ${common_files}/start_freej2me.sh ${system_root}/usr/bin/start_freej2me.sh
+chmod 775 ${system_root}/usr/bin/start_freej2me.sh
+cp -f ${common_files}/freej2me.sh ${system_root}/usr/bin/freej2me.sh
+chmod 775 ${system_root}/usr/bin/freej2me.sh
+cp -f ${common_files}/es_systems.cfg ${system_root}/usr/config/emulationstation/
+
+#disable nds unless you have your own build customized libSDL2 lib.
+#echo "Update nds files"
+#cp -rf ${common_files}/bg ${system_root}/usr/config/drastic/
+#mkdir -p ${system_root}/usr/config/drastic/lib/
+#cp -f ${common_files}/libSDL2-2.0.so.0 ${system_root}/usr/config/drastic/lib/
+#chmod 775 ${system_root}/usr/config/drastic/lib/libSDL2-2.0.so.0
+#cp -f ${common_files}/drastic ${system_root}/usr/config/drastic/
+#chmod 775 ${system_root}/usr/config/drastic/
+#cp -f ${common_files}/start_drastic.sh ${system_root}/usr/bin/start_drastic.sh
+#chmod 775 ${system_root}/usr/bin/start_drastic.sh
+
+echo "Update RA core file"
+cp ${common_files}/fbneo_libretro.so ${system_root}/usr/lib/libretro/
+chmod 775 ${system_root}/usr/lib/libretro/fbneo_libretro.so
+cp ${common_files}/multiemu_libretro.so ${system_root}/usr/lib/libretro/
+chmod 775 ${system_root}/usr/lib/libretro/multiemu_libretro.so
+cp ${common_files}/pcsx_rearmed_rumble_32b_libretro.* ${system_root}/usr/lib/libretro/
+chmod 775 ${system_root}/usr/lib/libretro/pcsx_rearmed_rumble_32b_libretro.so
+cp ${common_files}/mamearcade_libretro.so ${system_root}/usr/lib/libretro/
+chmod 775 ${system_root}/usr/lib/libretro/mamearcade_libretro.so
+cp ${common_files}/gam4980_32b_libretro* ${system_root}/usr/lib/libretro/
+chmod 775 ${system_root}/usr/lib/libretro/gam4980_32b_libretro.so
+
+echo "Update bezels.sh"
+cp -f ${common_files}/bezels.sh ${system_root}/usr/bin/
+chmod 775 ${system_root}/usr/bin/bezels.sh
+
+echo "Fix mplayer"
+cp -f ${common_files}/start_mplayer.sh ${system_root}/usr/bin/start_mplayer.sh
+chmod 775 ${system_root}/usr/bin/start_mplayer.sh
+
+echo "Update issue file" 
+cp ${common_files}/issue ${system_root}/etc/
+
+echo "Compressing SYSTEM image"
+mksquashfs ${system_root} SYSTEM -comp gzip -b 524288
+rm ${mount_point}/SYSTEM
+mv SYSTEM ${mount_point}/SYSTEM
+
+sync
+
+echo "Unmounting Jelos data partition"
+umount ${loop_device}p3
+losetup -d ${loop_device}
+
+rm -rf ${system_root}
+rm -rf ${mount_point}
